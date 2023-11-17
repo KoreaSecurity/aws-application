8799d96babe5354c1c5ecc722005e56eccd80e14
Sang-Hoon Choi <csh0052@gmail.com>
2023-11-17 20:05:28 +0900

    Update carrier_wave.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,9 @@ if Rails.env.production?
       # Amazon S3용 설정
       :provider        => 'AWS',
       :region          => ENV['AWS_INTRO_SAMPLE_S3_REGION'],     # 예: 'ap-northeast-1'
-      :use_iam_profile => true
+      :use_iam_profile => true,
+      :aws_access_key_id =>"키ID" ,
+      :aws_secret_access_key=> "엑세스키"
     }
     config.fog_directory =  ENV['AWS_INTRO_SAMPLE_S3_BUCKET']
   end
